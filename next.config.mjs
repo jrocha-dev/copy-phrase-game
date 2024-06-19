cc14d2b863e10630ed6fbef2e8b8984334d49683
José Rocha <5177556+jrocha-dev@users.noreply.github.com>
2024-06-19 12:48:03 -0300

    chore: GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/copy-phrase-game",
+  trailingSlash: true,
+};
 
 export default nextConfig;
